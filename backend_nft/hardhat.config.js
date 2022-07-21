eaa3ae5a225855be8885cbd3ca27a9ee22aa0b98
downsd16 <downsd16@gmail.com>
2022-07-21 19:42:44 -0400

    remove scripts

--- backend_nft/hardhat.config.js
+++ backend_nft/hardhat.config.js
@@ -10,7 +10,7 @@
 
 require("@nomiclabs/hardhat-ethers");
 
-const TEST1_PK = "e424694ae5236c16496783e5e8fc7183899bf7f5fad654b1b1c1d62d5d325cbf"
+const TEST1_PK = "no no no, you didn't say the magic word"
 const API_URL = "https://eth-rinkeby.alchemyapi.io/v2/k6YWwXNqqI4RjKRe--6p9D4sPQiZJySK"
 
 module.exports = {
