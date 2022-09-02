fac30f4bd1855c590c2a187c3d3ec51a31e49ccc
Devin Downs <64713709+downsd16@users.noreply.github.com>
2022-09-02 14:47:18 -0400

    Update frontend_test.js

--- backend_nft/scripts/frontend_test.js
+++ backend_nft/scripts/frontend_test.js
@@ -5,9 +5,9 @@
 const { expect } = require("chai");
 const fs = require("fs");
 
-const HARDHAT_NODE_PK_OWNER = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-const MM_TEST1_ADDRESS = "0xDeD8a8dADdf33F6F11dA36Ec155EfFD3D43fa99E";
-const DEPLOY_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+const HARDHAT_NODE_PK_OWNER = "";
+const MM_TEST1_ADDRESS = "";
+const DEPLOY_ADDRESS = "";
 const AccessController = JSON.parse(
   fs.readFileSync(
     "./artifacts/contracts/controller.sol/accessController.json",
